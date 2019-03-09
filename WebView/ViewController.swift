a72f64d9b63c1329151e03360fb8d5fafdd2340d
Hisman <hisman.ns@gmail.com>
2019-03-09 14:32:38 +0700

    Add WKWebview

--- WebView/ViewController.swift
+++ WebView/ViewController.swift
@@ -7,14 +7,57 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+    
+    private let website = "https://hisman.co"
+    
+    private let webView: WKWebView = {
+        let webConfiguration = WKWebViewConfiguration()
+        let wkWebView = WKWebView(frame: .zero, configuration: webConfiguration)
+        
+        wkWebView.isOpaque = false
+        wkWebView.backgroundColor = .white
+        wkWebView.allowsLinkPreview = false
+        wkWebView.allowsBackForwardNavigationGestures = true
+        wkWebView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return wkWebView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = .white
+        view.addSubview(webView)
+        
+        setupLayout()
+        loadWebView()
     }
-
+    
+    private func setupLayout() {
+        if #available(iOS 11.0, *) {
+            webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+            webView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        } else {
+            webView.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor).isActive = true
+            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        }
+        
+        webView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        webView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+    }
+    
+    private func loadWebView() {
+        let url = URL(string: website)
+        
+        if let url = url {
+            let request = URLRequest(url: url)
+            
+            webView.load(request)
+        }
+    }
 
 }
 
